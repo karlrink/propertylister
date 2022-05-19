524ccce02d7ebf16ad1a442aea73cf6363319b6f
karl <karl.rink@nationsinfocorp.com>
2022-05-18 17:04:28 -0700

    🌎 property lister 2022-05-18 v1

--- propertylister.js
+++ propertylister.js
@@ -1,5 +1,5 @@
 
-const version = '🌎 property lister 2022-05-18 v0';
+const version = '🌎 property lister 2022-05-18 v1';
 
 /* 
  * SPA (Single-Page Application)
@@ -188,8 +188,14 @@ function viewSubmit() {
       <input type="text" id="amount" name="amount" placeholder="$1.00">
       <br>
 
+      <br>
+
       <label for="street_address">street_address:</label>
       <input type="text" id="street_address" name="street_address" placeholder="22718 Haynes Street">
+      <br>
+
+      <label for="unit_number">unit_number:</label>
+      <input type="text" id="unit_number" name="unit_number" placeholder="apt #13 (Optional)" value="">
       <br>
 
       <label for="city">city:</label>
@@ -197,7 +203,7 @@ function viewSubmit() {
       <br>
 
       <label for="county">county:</label>
-      <input type="text" id="county" name="county" placeholder="Los Angeles">
+      <input type="text" id="county" name="county" placeholder="Los Angeles (Optional)" value="">
       <br>
 
       <label for="state_or_province">state_or_province:</label>
@@ -208,10 +214,27 @@ function viewSubmit() {
       <input type="text" id="postal_code" name="postal_code" placeholder="91307">
       <br>
 
+      <br>
       <button type="submit">Submit</button>
+
+      <hr>
+
+
+      <label for="description">description:</label>
+      <input type="text" id="description" name="description" placeholder="description (Optional)">
+      <br>
+
+      <label for="description">bedrooms:</label>
+      <input type="text" id="bedrooms" name="bedrooms" placeholder="bedrooms (Optional)">
+      <br>
+
+      <br>
+
     </form>
 
     </div>
+
+    <div id="form-output"></div>
 
     `;
 
@@ -222,7 +245,244 @@ function viewSubmit() {
     history.pushState({page: 'submit'}, "submit", "?view=submit");
 }
 
+window.submitForm = submitForm;
+async function submitForm(event) {
 
+    event.preventDefault();
+
+
+    // get all the values from the form
+    const amount = event.target['amount'].value;
+
+    const street_address = event.target['street_address'].value;
+    const unit_number = event.target['unit_number'].value;
+    const city = event.target['city'].value;
+    const county = event.target['county'].value;
+    const state_or_province = event.target['state_or_province'].value;
+    const postal_code = event.target['postal_code'].value;
+
+    // property_id = street_address + city + county + state_or_province + postal_code + unit_number
+    const property_string = street_address + city + county + state_or_province + postal_code + unit_number
+
+    const description = event.target['description'].value;
+    const bedrooms = event.target['bedrooms'].value;
+
+
+
+
+
+    const opensearch_data =
+{
+  "property_id": null,
+  "property_merged_id": null,
+  "scrape_date": null,
+  "list_date": null,
+  "data_source_listing_id": null,
+  "first_merge_date": null,
+  "sale_type": null,
+  "data_sources": null,
+  "amount_definition": null,
+  "amount": null,
+  "list_price": null,
+  "original_loan_amount": null,
+  "estimated_value": null,
+  "opening_bid": null,
+  "transfer_value": null,
+  "rent": null,
+  "mls_number": null,
+  "mls_disclaimer": null,
+  "parcel_number": null,
+  "unit_number": null,
+  "street_address": null,
+  "city": null,
+  "county": null,
+  "state_or_province": null,
+  "postal_code": null,
+  "property_record_type": null,
+  "bedrooms": null,
+  "baths_total": null,
+  "description": null,
+  "features": null,
+  "picture_count": null,
+  "picture_data_url": null,
+  "picture_data_source_url": null,
+  "picture_remote": null,
+  "listing_url": null,
+  "source_url": null,
+  "living_area_square_feet": null,
+  "living_area": null,
+  "lot_size": null,
+  "hoa_fee": null,
+  "parking": null,
+  "year_built": null,
+  "school_district": null,
+  "elementary_school": null,
+  "middle_school": null,
+  "high_school": null,
+  "foundation": null,
+  "room_list": null,
+  "appliances": null,
+  "floor_covering": null,
+  "roof_type": null,
+  "view_type": null,
+  "latitude": null,
+  "longitude": null,
+  "fips": null,
+  "condition": null,
+  "stories": null,
+  "total_units": null,
+  "total_rooms": null,
+  "family_room": null,
+  "living_room": null,
+  "den": null,
+  "kitchen": null,
+  "basement": null,
+  "heating": null,
+  "cooling": null,
+  "trust_deed_document_number": null,
+  "nod_date_defaulted_lien": null,
+  "nod_doc_number_defaulted_lien": null,
+  "nod_date_default": null,
+  "nod_amount_default": null,
+  "nod_recording_date": null,
+  "nod_recording_year": null,
+  "nod_document_number": null,
+  "auction_date": null,
+  "auction_time": null,
+  "nots_trustee_sale_number": null,
+  "nots_auction_title": null,
+  "nots_auction_address": null,
+  "nots_auction_city": null,
+  "nots_auction_state": null,
+  "nots_auction_zip": null,
+  "nots_auction_recording_date": null,
+  "nots_auction_document_number": null,
+  "nots_loan_date": null,
+  "nots_loan_no": null,
+  "nots_loan_default_amount": null,
+  "nots_auction_house_name": null,
+  "nots_auction_description": null,
+  "nots_auction_terms": null,
+  "nots_index_no": null,
+  "reo_document_number": null,
+  "judgment_amount": null,
+  "legal_description": null,
+  "zoning": null,
+  "lis_pendens_index_no": null,
+  "lis_pendens_recording_date": null,
+  "lis_pendens_loan_date": null,
+  "lis_pendens_docket_no": null,
+  "lis_pendens_doc_no": null,
+  "lis_pendens_loan_no": null,
+  "lis_pendens_case_no": null,
+  "hud_203k_eligible": null,
+  "hud_bid_deadline": null,
+  "hud_priority": null,
+  "hud_sale_status": null,
+  "hud_escrow_amount": null,
+  "tax_sale_jurisdiction_name": null,
+  "tax_sale_how_often": null,
+  "tax_sale_rate": null,
+  "tax_sale_redemption_period": null,
+  "tax_sale_bid_method": null,
+  "tax_sale_get_premium_back": null,
+  "tax_sale_sale_cert_number": null,
+  "assessed_valuation": null,
+  "assessed_valuation_improvement": null,
+  "assessed_valuation_land": null,
+  "assessed_tax_year": null,
+  "assessed_tax": null,
+  "assessed_year_delinquent": null,
+  "assessed_valuation_year": null,
+  "assessed_valuation_date": null,
+  "close_date": null,
+  "close_price": null,
+  "quality_score_sales": null,
+  "quality_score_member": null,
+  "quality_score_1": null,
+  "quality_score_2": null,
+  "quality_score_3": null,
+  "quality_score_4": null,
+  "quality_score_5": null,
+  "quality_score_6": null,
+  "quality_score_7": null,
+  "quality_score_8": null,
+  "quality_score_9": null,
+  "quality_score_10": null,
+  "home_score": null,
+  "investor_score": null,
+  "listing_status": null,
+  "tracking": null,
+  "reo_recording_date": null,
+  "address_id": null,
+  "avm_low": null,
+  "avm_high": null,
+  "avm_confidence": null,
+  "avm_date": null,
+  "estimated_rent": null,
+  "monthly_amount_RTO_cove_algo": null,
+  "monthly_amount_RTO_henrys_algo": null,
+  "monthly_amount_30year_fixed_mortgage": null,
+  "monthly_amount_5x1_arm_mortgage": null,
+  "home_price_for_monthly": null,
+  "full_street_name": null,
+  "assessed_valuation_rate": null,
+  "parking_spaces": null,
+  "pool": null,
+  "style": null,
+  "construction_type": null,
+  "exterior_walls": null,
+  "sort_amount": null,
+  "delete_reason": null,
+  "estimated_monthly_payment": null,
+  "paid_listing": null,
+  "frstlk_exp_date": null,
+  "100_dwn_pymnt": null,
+  "hmpth_mrtge": null,
+  "hmpth_renvtion_mrtge": null,
+  "price_reduction": null,
+  "nsp_home": null,
+  "gnnd_exp_date": null,
+  "specialty_home": null,
+  "full_details": null,
+  "alt_finance": null,
+  "exclusive": null,
+  "special_financing": null,
+  "bargain_price": null,
+  "fixer_upper": null,
+  "rto_financing": null,
+  "rto_potential": null,
+  "FA_property_ID": null,
+  "building_amenities": null,
+  "disabled_ind": null,
+  "geo_status_code": null,
+  "lot_size_acres": null,
+  "lot_size_depth_feet": null,
+  "lot_size_frontage_feet": null,
+  "owner_occupied": null,
+  "prop_class_id": null,
+  "veteran_ind": null,
+  "widow_ind": null,
+  "DTDB_Ref_ID": null,
+  "coordinate": {
+    "lat": null,
+    "lon": null
+  }
+}
+
+
+    let htmlSegment = '';
+
+
+    document.querySelector('#form-output').innerHTML = htmlSegment;
+
+    history.pushState({page: 'submit'}, "submit", "?view=submit&true");
+
+}
+
+
+// property_id = street_address + city + county + state_or_province + postal_code + unit_number
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 
 
 function viewInfo() {
